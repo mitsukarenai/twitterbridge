7420f886f23ba03ea82c5146275dd5722f387877
Mitsukarenai <mitsu@suumitsu.eu>
2013-03-14 11:43:25 +0100

    new file:   config.php 	modified:   index.php 	new file:   whitelist.json Data must flow, dear Twitter. Separate config, credentials control, process by JSON whitelist, JSON output, portable cachefile structure.

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+$config = array(
+  'directory' => '', //The path used to store the "cache.json" cache file.
+
+/* BEGIN Twitter credentials */
+// 		sign up a new app:  https://dev.twitter.com/apps/new  (with access token) and enter the values:
+  'key' => '',
+  'secret' => '',
+  'token' => '',
+  'token_secret' => '',
+//  - END Twitter API credentials
+
+  'cache_expire' => 900 //The duration of the cache  
+);
+
+?>
--- index.php
+++ index.php
@@ -1,62 +1,76 @@
 <?php
 // original: https://github.com/stormuk/storm-twitter + stevelacey patch
-if(!isset($_GET['u'])) { header("HTTP/1.0 403 Forbidden"); die('no username provided');}
+if(!isset($_GET['u'])) { header("HTTP/1.0 404 Not Found"); die('no username provided');}
 $screenName = $_GET['u'];
 
 require('StormTwitter.class.php');
+require('config.php');
 
-$config = array(
-  'directory' => '', //The path used to store the .tweetcache cache file.
-// sign up a new app:  https://dev.twitter.com/apps/new
-  'key' => '',
-  'secret' => '',
-  'token' => '',
-  'token_secret' => '',
-  'cache_expire' => 3600 //The duration of the cache  
-);
+// configuration control
+if($config['key'] == '' or $config['secret'] == '' or $config['token'] == '' or $config['token_secret'] == '') { header("HTTP/1.0 404 Not Found"); die('missing API credentials in config.php'); }
 
+// whitelist control
+$whitelist = json_decode(file_get_contents('whitelist.json'), TRUE);
+if (!in_array($screenName, $whitelist)) { header("HTTP/1.0 403 Forbidden"); die('username is not whitelisted'); }
+
+/*  START PROCESSING */
 $twitter = new StormTwitter($config);
 
 // getTweets is the only public method. For legacy reasons, it takes between 0 and 3 parameters.
 // getTweets(twitter_screenname, number_of_tweets, custom_parameters_to_go_twitter); 
 
-$twitter_data = $twitter->getTweets($screenName, 100, array('include_rts'=>true,'exclude_replies'=>true));
+$twitter_data = $twitter->getTweets($screenName, 20, array('include_rts'=>true,'exclude_replies'=>true));
 
-// $rss is the HTML output string
-$rss = '<?xml version="1.0" encoding="UTF-8"?><rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/">';
-$rss .= '<channel><title>Twitter / '.$screenName.'</title><link>http://twitter.com/'.$screenName.'</link><description>Twitter updates from '.$screenName.'.</description>';
- 
-for ($i = 0; $i < 20; $i++) {
-// Tweet Text
-$desc = $twitter_data[$i]['text'];
-// Build link back
-$link = $twitter_data[$i]['id_str'];
-// Date tweet posted
-$date = $twitter_data[$i]['created_at'];
-$date = strtotime($date);
- 
- 
-// Build final output
-if(!empty($link))
-	{ 
-	$rss .=	'
-	<item>
-	<title>'.$desc.'</title>
-	<description>'.$desc.'</description>
-	<pubDate>'.date(DATE_RSS, $date).'</pubDate>
-	<guid>http://twitter.com/'.$screenName.'/statuses/'.$link.'</guid>
-	<link>http://twitter.com/'.$screenName.'/statuses/'.$link.'</link>
-	</item>';
+
+
+if($_GET['format'] == 'json')
+	{
+	/* JSON OUTPUT */
+	header('Content-Type: application/json');
+	echo json_encode($twitter_data);
+	die;
 	}
+
+else
+	{
+	/*  RSS OUTPUT */
+	// $rss is the HTML output string
+	$rss = '<?xml version="1.0" encoding="UTF-8"?><rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/">';
+	$rss .= '<channel><title>Twitter / '.$screenName.'</title><link>http://twitter.com/'.$screenName.'</link><description>Twitter updates from '.$screenName.'.</description>';
  
-}
+	for ($i = 0; $i < 21; $i++) {
+		// Tweet Text
+		$desc = $twitter_data[$i]['text'];
+		// Build link back
+		$link = $twitter_data[$i]['id_str'];
+		// Date tweet posted
+		$date = $twitter_data[$i]['created_at'];
+		$date = strtotime($date);
 
-// Final touch
-$rss .= '
-</channel>
-</rss>';
+		// Build final output
+		if(!empty($link))
+			{ 
+			$rss .=	'
+			<item>
+			<title>'.$desc.'</title>
+			<description>'.$desc.'</description>
+			<pubDate>'.date(DATE_RSS, $date).'</pubDate>
+			<guid>http://twitter.com/'.$screenName.'/statuses/'.$link.'</guid>
+			<link>http://twitter.com/'.$screenName.'/statuses/'.$link.'</link>
+			</item>';
+			}
+ 
+		}
 
-// Send the stuff
-header('Content-Type: application/rss+xml; charset=utf-8');
-echo $rss;
+	// Final touch
+	$rss .= '
+	</channel>
+	</rss>';
+
+	// Send the stuff
+	header('Content-Type: application/rss+xml; charset=utf-8');
+	echo $rss;
+	die;
+	} /* END RSS OUTPUT */
+
 ?>
